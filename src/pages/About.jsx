34466fb7170a09aaf0b944c51d0d3768f69d7ca1
Aayush K Agarwal <aayushagarwal685@gmail.com>
2024-08-01 17:00:44 +0530

    added about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,17 +8,15 @@ export default function About() {
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
+              Welcome to Quote Rider Blog! This blog was created by Aayush k Agarwal
               as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              world.
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              On this blog, you'll find weekly posts on topics
+              such as I don't know. Aayush who own Quote Rider is always learning and exploring new
+              stuff, so be sure to check back often for new content!
             </p>
 
             <p>
