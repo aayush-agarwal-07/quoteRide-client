827a825f735445729e1d0846abfdab550b5db9b3
Aayush K Agarwal <aayushagarwal685@gmail.com>
2024-08-09 11:57:15 +0530

    implement firebase urls to my videos

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   base: "/", // Ensure this is set to the correct base path if needed
   build: {
     outDir: "dist",
     assetsDir: "assets", // Ensure assets are placed in the 'assets' directory
+    chunkSizeWarningLimit: 2000, // Adjust the size limit (in KB) as needed
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
-  plugins: [react()],
+  plugins: [react(), visualizer()],
 });
